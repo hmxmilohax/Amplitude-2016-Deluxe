28505ed51d518a0fa069592811df9131f20e729f
LlysiX <smyler02@gmail.com>
2022-09-24 17:19:18 -0700

    emu unlock fps bat script

---
+++ _emu_unlock_fps.bat
@@ -0,0 +1,3 @@
+del "%~dp0_ark\ps3\system\data\config\default.dta"
+copy "%~dp0dx_optional_additions\emu_unlock_fps\default.dta" "%~dp0_ark\ps3\system\data\config"
+pause
---
+++ _lock_fps.bat
@@ -0,0 +1,3 @@
+del "%~dp0_ark\ps3\system\data\config\default.dta"
+copy "%~dp0dx_optional_additions\_revert_to_default\default.dta" "%~dp0_ark\ps3\system\data\config"
+pause
