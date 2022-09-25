25f177df67e03cfbadbd0d51d05237d45a5f022f
LlysiX <smyler02@gmail.com>
2022-09-24 17:12:38 -0700

    delete non-full game dtb<->dta scripts

--- dev_scripts/_dtb2a-ps3.bat
+++
@@ -1,2 +0,0 @@
-FOR /F "tokens=*" %%G IN ('dir /b *.dta_dta_ps3') DO dtxtool\dtxtool dtb2a "%%G" "%%~nG.dta"
-FOR /F "tokens=*" %%G IN ('dir /b *.script_dta_ps3') DO dtxtool\dtxtool dtb2a "%%G" "%%~nG.script_dta"
