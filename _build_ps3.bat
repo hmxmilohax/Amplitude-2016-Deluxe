13c6f5f7e7d0e94d445eaf86651d1d8e01b244fa
LysiX <smyler02@gmail.com>
2022-09-27 16:09:11 -0700

    "combined" ark folder and more build fixes

--- _build_ps3.bat
+++ _build_ps3.bat
@@ -1,8 +1,10 @@
 git pull https://github.com/hmxmilohax/amplitude-2016-deluxe main
+xcopy /e _ark\combined _ark\ps3
 for /R "%~dp0_ark\ps3" %%f in (*) do (if "%%~xf"==".dta_dta_ps3" del "%%~f")
 for /R "%~dp0_ark\ps3" %%f in (*) do (if "%%~xf"==".script_dta_ps3" del "%%~f")
 for /R "%~dp0_ark\ps3" %%f in (*.dta) do dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.dta_dta_ps3" 3
 for /R "%~dp0_ark\ps3" %%f in (*.script_dta) do dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.script_dta_ps3" 3
+xcopy /e /y _ark\ps3 _build\modulate\ps3\ps3
 cd _build\modulate
 modulate -ps3 -autoadd ps3 -pack_add ps3 ../ps3
 pause
--- _build_ps4.bat
+++ _build_ps4.bat
@@ -1,8 +1,10 @@
 git pull https://github.com/hmxmilohax/amplitude-2016-deluxe main
+xcopy /e _ark\combined _ark\ps4
 for /R "%~dp0_ark\ps4" %%f in (*) do (if "%%~xf"==".dta_dta_ps4" del "%%~f")
 for /R "%~dp0_ark\ps4" %%f in (*) do (if "%%~xf"==".script_dta_ps4" del "%%~f")
 for /R "%~dp0_ark\ps4" %%f in (*.dta) do dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.dta_dta_ps4" 3
 for /R "%~dp0_ark\ps4" %%f in (*.script_dta) do dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.script_dta_ps4" 3
+xcopy /e /y _ark\ps4 _build\modulate\ps4\ps4
 cd _build\modulate
-modulate -autoadd ps4 -pack_add ps4 ../ps4
+rem modulate -autoadd ps4 -pack_add ps4 ../ps4
 pause
