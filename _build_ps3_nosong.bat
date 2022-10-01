e3acf2fc29a72b7b26a5723178be5039918e8c34
LysiX <smyler02@gmail.com>
2022-10-01 15:54:10 -0700

    build to usrdir

--- _build_ps3_nosong.bat
+++ _build_ps3_nosong.bat
@@ -5,5 +5,5 @@ for /R "%~dp0_ark\ps3" %%f in (*.dta) do dependencies\dtxtool\dtxtool dta2b "%%f
 for /R "%~dp0_ark\ps3" %%f in (*.script_dta) do dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.script_dta_ps3" 3
 xcopy /e /y _ark\ps3 _build\modulate\ps3\ps3
 cd _build\modulate
-modulate -ps3 -autoadd ps3 -pack_add ps3 ../ps3
+modulate -ps3 -autoadd ps3 -pack_add ps3 ../ps3/USRDIR
 pause
