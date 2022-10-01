2219ac914a35e6befc1a178371ee35784c8cdfb8
LysiX <smyler02@gmail.com>
2022-10-01 13:22:26 -0700

    update customs folder

--- dependencies/download_songs_ps4.py
+++ dependencies/download_songs_ps4.py
@@ -4,7 +4,7 @@ import shutil
 import os
 try:
     import git
-    print("module 'git' is installed. Downloading/enabling additional amp 2016 custom songs, this may take some time.")
+    print("module 'git' is installed. Downloading/enabling additional Amp 2016 custom songs for PS4, this may take some time.")
 except ModuleNotFoundError:
     print("module 'git' is not installed. Install it via '/dependencies/install_gitpython.bat' or 'pip install gitpython'")
     sys.exit(1)
@@ -13,7 +13,7 @@ except ModuleNotFoundError:
 cwd = Path().absolute()
 
 # clone/pull amp songs
-amp_songs_source_path = cwd.joinpath("amp-2016-customs")
+amp_songs_source_path = cwd.joinpath("amp-2016-customs-ps4")
 
 try:
     repo = git.Repo.clone_from("https://github.com/jnackmclain/amp-2016-customs.git", amp_songs_source_path, branch="ps4")
@@ -22,7 +22,7 @@ except:
     origin = repo.remotes.origin
     origin.pull()
 
-amp_2016_songs_source_folder = cwd.joinpath("amp-2016-customs/songs")
+amp_2016_songs_source_folder = cwd.joinpath("amp-2016-customs-ps4/songs")
 amp_2016_customs_folder = cwd.joinpath("_ark/ps4/songs")
 files = os.listdir(amp_songs_source_path)
 shutil.copytree(amp_2016_songs_source_folder, amp_2016_customs_folder)
