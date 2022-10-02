40403ba96a77e7191a1202cd1e1420cb6b98e03e
LysiX <smyler02@gmail.com>
2022-10-01 18:02:44 -0700

    create USRDIR folder on first build

--- _build_ps3.bat
+++ _build_ps3.bat
@@ -3,6 +3,8 @@ cd _build\modulate
 mkdir ps3
 modulate -ps3 -unpack ps3
 cd ..\..
+mkdir _build\ps3\USRDIR
+pause
 git pull https://github.com/hmxmilohax/amplitude-2016-deluxe main
 xcopy /e /y _ark\combined _ark\ps3
 for /R "%~dp0_ark\ps3" %%f in (*.dta) do dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.dta_dta_ps3" 3
