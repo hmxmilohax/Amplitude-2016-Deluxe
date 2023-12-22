5dd627fe38ebece55d039976b57e0b843f073607
LysiX <smyler02@gmail.com>
2023-12-22 14:19:56 -0800

    update prep location

--- _build_ps3.bat
+++ _build_ps3.bat
@@ -2,21 +2,21 @@
 mkdir _build\ps3\USRDIR
 mkdir _tmpbuild
 git pull https://github.com/hmxmilohax/amplitude-2016-deluxe main
-IF NOT EXIST "%~dp0_build\prep\ps3\ps3" CALL dev_scripts\!prep_ps3.bat
+IF NOT EXIST "%~dp0_prep_ps3\ext_ark\ps3" CALL dev_scripts\!prep_ps3.bat
 echo:Copying Amplitude 2016 Deluxe PS3 files...
 xcopy /q /e /y _ark\ps3 _tmpbuild
 xcopy /q /e /y _ark\combined _tmpbuild
 for /R "%~dp0_tmpbuild" %%f in (*.dta) do IF NOT "%%~xf" == ".dta_dta_ps3" dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.dta_dta_ps3" 3
-for /R "%~dp0_tmpbuild" %%f in (*.script_dta) do IF NOT "%%~xf" == ".script_dta_ps3" dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.script_dta_ps3" 3
+for /R "%~dp0_tmpbuild" %%f in (*.script) do IF NOT "%%~xf" == ".script_dta_ps3" dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.script_dta_ps3" 3
 for /R "%~dp0_tmpbuild" %%f in (*.dta) do IF NOT "%%~xf" == ".dta_dta_ps3" del "%%f"
-for /R "%~dp0_tmpbuild" %%f in (*.script_dta) do IF NOT "%%~xf" == ".script_dta_ps3" del "%%f"
-xcopy /q /e /y _tmpbuild _build\prep\ps3\ps3
+for /R "%~dp0_tmpbuild" %%f in (*.script) do IF NOT "%%~xf" == ".script_dta_ps3" del "%%f"
+xcopy /q /e /y _tmpbuild _prep_ps3\ext_ark\ps3
 for /R "%~dp0_tmpbuild" %%f in (*) do del "%%f"
 rmdir /s /q "%~dp0_tmpbuild"
 echo:Adding songs to Amplitude 2016 Deluxe config...
-"%~dp0dependencies\amphelper" song add-all "%~dp0_build\prep\ps3" >nul
+"%~dp0dependencies\amphelper" song add-all "%~dp0_prep_ps3\ext_ark" >nul
 echo:Building Amplitude 2016 Deluxe arks...
-"%~dp0dependencies\amphelper" ark pack "%~dp0_build\prep\ps3" "%~dp0_build\ps3\USRDIR\main_ps3.hdr" >nul
+"%~dp0dependencies\amphelper" ark pack "%~dp0_prep_ps3\ext_ark" "%~dp0_build\ps3\USRDIR\main_ps3.hdr" >nul
 echo:Wrote Amplitude 2016 Deluxe arks.
 echo:Complete! Enjoy Amplitude 2016 Deluxe
 pause
--- dev_scripts/!prep_ps4.bat
+++ dev_scripts/!prep_ps4.bat
@@ -1,8 +1,8 @@
 @echo off
 echo:Preparing to build PS4 For the first time...
-echo:Make sure your 1.0 vanilla ark files are in _build/prep/
-cd "%~dp0..\_build\prep"
-mkdir ps4
-cd ..\..
-"%~dp0..\dependencies\amphelper" ark unpack "%~dp0..\_build\prep\main_ps4.hdr" "%~dp0..\_build\prep\ps4" >nul
+echo:Make sure your 1.0 vanilla ark files are in _prep_ps4
+cd "%~dp0..\_prep_ps4"
+mkdir ext_ark
+cd "%~dp0..\"
+"%~dp0..\dependencies\amphelper" ark unpack "%~dp0..\_prep_ps4\main_ps4.hdr" "%~dp0..\_prep_ps4\ext_ark" >nul
 echo:Amplitude 2016 Deluxe is now ready to be built!
