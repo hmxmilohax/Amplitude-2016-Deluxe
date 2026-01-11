54d0b937a72f39665be85b834a251ec035fa66c6
LysiX <smyler02@gmail.com>
2026-01-10 23:28:59 -0600

    update nosong build bats to speed up build times

--- _build_ps3_nosong.bat
+++ _build_ps3_nosong.bat
@@ -4,8 +4,7 @@ mkdir _tmpbuild
 IF NOT EXIST "%~dp0_prep_ps3\ext_ark\ps3" CALL dev_scripts\!prep_ps3.bat
 echo:Copying Amplitude 2016 Deluxe PS3 files...
 xcopy /q /e /y _ark\ps3 _tmpbuild
-xcopy /q /e /y _ark\combined _tmpbuild
-rmdir /s /q _tmpbuild\songs
+robocopy _ark\combined _tmpbuild /e /xd "songs" >nul
 for /R "%~dp0_tmpbuild" %%f in (*.dta) do IF NOT "%%~xf" == ".dta_dta_ps3" dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.dta_dta_ps3" 3
 for /R "%~dp0_tmpbuild" %%f in (*.script) do IF NOT "%%~xf" == ".script_dta_ps3" dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.script_dta_ps3" 3
 for /R "%~dp0_tmpbuild" %%f in (*.dta) do IF NOT "%%~xf" == ".dta_dta_ps3" del "%%f"
@@ -14,6 +13,7 @@ xcopy /q /e /y _tmpbuild _prep_ps3\ext_ark\ps3\
 for /R "%~dp0_tmpbuild" %%f in (*) do del "%%f"
 rmdir /s /q "%~dp0_tmpbuild"
 echo:Building Amplitude 2016 Deluxe arks without new songs...
+"%~dp0dependencies\amphelper" song add-all "%~dp0_prep_ps3\ext_ark" >nul
 "%~dp0dependencies\amphelper" ark pack "%~dp0_prep_ps3\ext_ark" "%~dp0_build\ps3\USRDIR\main_ps3.hdr" >nul
 echo:Wrote Amplitude 2016 Deluxe arks.
 echo:Complete! Enjoy Amplitude 2016 Deluxe
--- _build_ps4_nosong.bat
+++ _build_ps4_nosong.bat
@@ -4,8 +4,7 @@ mkdir _tmpbuild
 IF NOT EXIST "%~dp0_prep_ps4\ext_ark\ps4" CALL dev_scripts\!prep_ps4.bat
 echo:Copying Amplitude 2016 Deluxe PS4 files...
 xcopy /q /e /y _ark\ps4 _tmpbuild
-xcopy /q /e /y _ark\combined _tmpbuild
-rmdir /s /q _tmpbuild\songs
+robocopy _ark\combined _tmpbuild /e /xd "songs" >nul
 for /R "%~dp0_tmpbuild" %%f in (*.dta) do IF NOT "%%~xf" == ".dta_dta_ps4" dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.dta_dta_ps4" 3
 for /R "%~dp0_tmpbuild" %%f in (*.script) do IF NOT "%%~xf" == ".script_dta_ps4" dependencies\dtxtool\dtxtool dta2b "%%f" "%%~dpnf.script_dta_ps4" 3
 for /R "%~dp0_tmpbuild" %%f in (*.dta) do IF NOT "%%~xf" == ".dta_dta_ps4" del "%%f"
@@ -14,6 +13,7 @@ xcopy /q /e /y _tmpbuild _prep_ps4\ext_ark\ps4\
 for /R "%~dp0_tmpbuild" %%f in (*) do del "%%f"
 rmdir /s /q "%~dp0_tmpbuild"
 echo:Building Amplitude 2016 Deluxe arks without new songs...
+"%~dp0dependencies\amphelper" song add-all "%~dp0_prep_ps4\ext_ark" >nul
 "%~dp0dependencies\amphelper" ark pack "%~dp0_prep_ps4\ext_ark" "%~dp0_build\ps4\AFR\CUSA02480\main_ps4.hdr" >nul
 echo:Wrote Amplitude 2016 Deluxe arks.
 echo:Complete! Enjoy Amplitude 2016 Deluxe
